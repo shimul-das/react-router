2a7a63c29a733cff166f4f42a402d63b5857cdba
Shimul Das <shimul.191002110@gmail.com>
2023-04-08 08:55:09 +0600

    add link to remove loading

--- src/assets/Component/Header/Header.jsx
+++ src/assets/Component/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
-  return (
-    <div>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </div>
-  )
+return (
+// <div>
+    // <a href="/">Home</a>
+    // <a href="/about">About</a>
+    // <a href="/contact">Contact</a>
+    // </div>
+    //use to to remove load
+<div>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+</div>
+)
 }
